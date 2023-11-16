8f964b0b3d9bf869290eb034b88f6d72f13b550a
Sidhima7 <147191733+Sidhima7@users.noreply.github.com>
2023-11-17 00:52:26 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the size of the array");
+    scanf("%d",&n);
+    int a[n],i,sum=0;
+    printf("Enter the array elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+    printf("Sum of the array=%d",sum);
+    return 0;
+}
